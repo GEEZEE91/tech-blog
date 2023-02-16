941b4e252f85e9a30fd4561eb48530a75f330d08
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-02-16 15:27:00 +0800

    fix router issues

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// 2 Establish Server Port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,12 +17,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -35,12 +31,13 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// 3 Use express and Sequelize to access the database through the routes (through controllers/index.js) (see config/connection.js)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ alter: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
