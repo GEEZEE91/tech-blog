99500e56ac204cd88e911e3a607c98e21e0151c1
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-02-16 15:13:12 +0800

    set up routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
-es (11 sloc)  374 Bytes
+
 
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
-const dashboardRoutes = require('./dashboardRoutes.js');
 
 router.use('/', homeRoutes);
-// router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use((req, res) => {
     res.status(404).end();
