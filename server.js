7f9d2c8c183f07ee9aa92439e39b42eb2e6a70b0
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-02-16 15:48:09 +0800

    refactor set up

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+var bodyParser = require('body-parser')
+
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// 2 Establish Server Port
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const PORT = process.env.PORT || 3010;
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,17 +19,20 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// 3 Use express and Sequelize to access the database through the routes (through controllers/index.js) (see config/connection.js)
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.text({ type: 'text/html' }))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,5 +40,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`************** Now listening on port ${PORT} ******************`));
 });
